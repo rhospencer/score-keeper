90b8f536bf95c9c6a0275c86887f40db6b6f368e
Spencer Rhoton <rhotonspencer@gmail.com>
2020-02-25 14:37:58 -0700

    mongo db set up

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
+const mongoose = require('mongoose')
 
 const authCtrl = require('./controllers/authController')
 
 const chalk = require('chalk')
 
-const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING, MONGO_CONNECTION_STRING} = process.env
 
 const app = express()
 
@@ -23,6 +24,16 @@ app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
 app.delete('/auth/logout', authCtrl.logout)
 app.get('/auth/me', authCtrl.userInfo)
+
+mongoose.connect(MONGO_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(dbInstance => {
+    app.set('mdb', dbInstance)
+    console.log(chalk.blue('mongo db connected'))
+})
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 massive(CONNECTION_STRING)
   .then(db => {
